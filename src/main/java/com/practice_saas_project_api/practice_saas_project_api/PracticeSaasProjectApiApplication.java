342c97d0bc02468f096447044f2ea7697a0ee477
AssimIsmail <ismailassimdev@gmail.com>
2025-03-20 12:06:11 +0000

    init

---
+++ src/main/java/com/practice_saas_project_api/practice_saas_project_api/PracticeSaasProjectApiApplication.java
@@ -0,0 +1,13 @@
+package com.practice_saas_project_api.practice_saas_project_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSaasProjectApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeSaasProjectApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice_saas_project_api/practice_saas_project_api/PracticeSaasProjectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice_saas_project_api.practice_saas_project_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeSaasProjectApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
